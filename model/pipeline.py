70335542db86238f52387665dd418f25ea0a6355
shashankag14 <shashankag14@gmail.com>
2024-04-30 13:29:10 +0200

    Add folder structure and dependencies
    tox command fails, code unstable

---
+++ model/pipeline.py
@@ -0,0 +1,55 @@
+# for encoding categorical variables
+from feature_engine.encoding import OneHotEncoder, RareLabelEncoder
+# for imputation
+from feature_engine.imputation import AddMissingIndicator, CategoricalImputer, MeanMedianImputer
+# for feature selection
+from feature_engine.selection import DropFeatures
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+# to build the models
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.pipeline import Pipeline
+
+from model.config.core import config
+from model.processing.features import ExtractLetterTransformer
+
+titanic_pipe = Pipeline([
+    # Drop features which do not help in prediction
+    ("drop_features", DropFeatures(features_to_drop=[config.model_config.vars_to_drop])),
+
+    # ===== IMPUTATION =====
+    # impute categorical variables with string missing
+    ('categorical_imputation', CategoricalImputer(
+        imputation_method='missing', 
+        variables=config.model_config.categorical_vars)),
+
+    # add missing indicator to numerical variables
+    ('missing_indicator', AddMissingIndicator(variables=config.model_config.numerical_vars)),
+
+    # impute numerical variables with the median
+    ('median_imputation', MeanMedianImputer(
+        imputation_method='median', 
+        variables=config.model_config.numerical_vars)),
+
+    # Extract letter from cabin
+    ('extract_letter', ExtractLetterTransformer(variables=config.model_config.vars_to_extract_letter)),
+
+    # == CATEGORICAL ENCODING ======
+    # remove categories present in less than 5% of the observations (0.05)
+    # group them in one category called 'Rare'
+    ('rare_label_encoder', RareLabelEncoder(
+        tol=0.05, 
+        n_categories=1, 
+        variables=config.model_config.categorical_vars)),
+
+    # encode categorical variables using one hot encoding into k-1 variables
+    ('categorical_encoder', OneHotEncoder(
+        drop_last=True, 
+        variables=config.model_config.categorical_vars)),
+
+    # scale
+    ('scaler', StandardScaler()),
+
+    ('Logit', LogisticRegression(C=0.0005, random_state=0)),
+])
---
+++ model/processing/data_manager.py
@@ -0,0 +1,88 @@
+import typing as t
+from pathlib import Path
+
+import re
+import joblib
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+
+from model import __version__ as _version
+from model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config
+
+def preprocess_dataset(*, dataframe: pd.DataFrame) -> pd.DataFrame:
+    
+    def get_first_cabin(row):
+        try:
+            return row.split()[0]
+        except:
+            return np.nan
+
+    def get_title(passenger):
+        line = passenger
+        if re.search('Mrs', line):
+            return 'Mrs'
+        elif re.search('Mr', line):
+            return 'Mr'
+        elif re.search('Miss', line):
+            return 'Miss'
+        elif re.search('Master', line):
+            return 'Master'
+        else:
+            return 'Other'
+
+    dataframe = dataframe.replace('?', np.nan)
+
+    dataframe['cabin'] = dataframe['cabin'].apply(get_first_cabin)
+    dataframe['title'] = dataframe['name'].apply(get_title)
+
+    dataframe['fare'] = dataframe['fare'].astype('float')
+    dataframe['age'] = dataframe['age'].astype('float')
+
+    # dataframe.drop(labels=[config.model_config.variables_to_drop], axis=1, inplace=True)
+
+    return dataframe
+
+def load_dataset(*, file_name: str) -> pd.DataFrame:
+    dataframe = pd.read_csv(Path(f"{DATASET_DIR}/{file_name}"))
+
+    # preprocess and clean the data before using it in the model
+    preprocessed_dataframe = preprocess_dataset(dataframe=dataframe)
+    return preprocessed_dataframe
+
+
+def save_pipeline(*, pipeline_to_persist: Pipeline) -> None:
+    """Persist the pipeline.
+    Saves the versioned model, and overwrites any previous
+    saved models. This ensures that when the package is
+    published, there is only one trained model that can be
+    called, and we know exactly how it was built.
+    """
+
+    # Prepare versioned save file name
+    save_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
+    save_path = TRAINED_MODEL_DIR / save_file_name
+
+    remove_old_pipelines(files_to_keep=[save_file_name])
+    joblib.dump(pipeline_to_persist, save_path)
+
+
+def load_pipeline(*, file_name: str) -> Pipeline:
+    """Load a persisted pipeline."""
+
+    file_path = TRAINED_MODEL_DIR / file_name
+    trained_model = joblib.load(filename=file_path)
+    return trained_model
+
+
+def remove_old_pipelines(*, files_to_keep: t.List[str]) -> None:
+    """
+    Remove old model pipelines.
+    This is to ensure there is a simple one-to-one
+    mapping between the package version and the model
+    version to be imported and used by other applications.
+    """
+    do_not_delete = files_to_keep + ["__init__.py"]
+    for model_file in TRAINED_MODEL_DIR.iterdir():
+        if model_file.name not in do_not_delete:
+            model_file.unlink()
