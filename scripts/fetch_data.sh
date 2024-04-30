7fd3fa30e197aeaf77a4bded0983a3cbb7690915
shashankag14 <shashankag14@gmail.com>
2024-04-30 14:07:16 +0200

    fix: missing data headers in train and test csv

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -26,9 +26,12 @@ TEST_CSV="$DEST_FOLDER/test.csv"
 TOTAL_LINES=$(wc -l < "$ORIGINAL_CSV")
 TRAIN_LINES=$((TOTAL_LINES * 80 / 100))
 
-# Shuffle the dataset lines randomly
-awk 'BEGIN {srand()} {print rand(), $0}' "$ORIGINAL_CSV" | sort -n | cut -d ' ' -f 2- > shuffled.csv
+# Get header line
+HEADER=$(head -n 1 "$ORIGINAL_CSV")
 
-# Use awk to split the shuffled CSV file into two files
-awk -v train_lines="$TRAIN_LINES" 'NR <= train_lines { print > "'"$TRAIN_CSV"'" } NR > train_lines { print > "'"$TEST_CSV"'" }' shuffled.csv
+# Shuffle the dataset lines randomly (excluding header)
+awk 'BEGIN {srand()} NR>1 {print rand(), $0}' "$ORIGINAL_CSV" | sort -n | cut -d ' ' -f 2- > shuffled.csv
+
+# Use awk to split the shuffled CSV file into two files (including header)
+awk -v header="$HEADER" -v train_lines="$TRAIN_LINES" 'NR == 1 { print header > "'"$TRAIN_CSV"'"; print header > "'"$TEST_CSV"'" } NR > 1 && NR <= train_lines+1 { print > "'"$TRAIN_CSV"'" } NR > train_lines+1 { print > "'"$TEST_CSV"'" }' shuffled.csv
 rm shuffled.csv
