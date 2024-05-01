9e3ba18f2b33850aea55cd06e5c9805224a19440
shashankag14 <shashankag14@gmail.com>
2024-05-01 19:38:47 +0200

    Add functional test for feature extraction and transformation

---
+++ tests/test_features.py
@@ -0,0 +1,16 @@
+from model.config.core import config
+from model.processing.features import ExtractLetterTransformer
+
+
+def test_temporal_variable_transformer(sample_input_data):
+    # Given
+    transformer = ExtractLetterTransformer(
+        variables=config.model_config.vars_to_extract_letter,  # cabin
+    )
+    assert sample_input_data["cabin"].iat[3] == "E33"
+
+    # When
+    subject = transformer.fit_transform(sample_input_data)
+
+    # Then
+    assert subject["cabin"].iat[3] == "E"
