7179d08cba351dab2753e80758f9f342fd279b5b
shashankag14 <shashankag14@gmail.com>
2024-05-01 19:38:05 +0200

    add accuracy tolerance in functional test for predictions

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -1,20 +1,19 @@
-import math
 import numpy as np
 
+from sklearn.metrics import accuracy_score
 from model.predict import make_prediction
 
 
 def test_prediction_against_truth_value(sample_input_data):
-    # Given
-    expected_first_prediction_value = 1
-
     # When
     estimated_results = make_prediction(input_data=sample_input_data)
 
     # Then
     predictions = estimated_results.get("predictions")
-    assert isinstance(predictions, list)
-    assert isinstance(predictions[0], np.float64)
+    assert isinstance(predictions, np.ndarray)
+    assert isinstance(predictions[0], np.int64)
     assert estimated_results.get("errors") is None
-    # assert len(predictions) == expected_no_predictions
-    assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=100)
+    _predictions = list(predictions)
+    y_true = sample_input_data["survived"]
+    accuracy = accuracy_score(y_true, _predictions)
+    assert accuracy > 0.7
